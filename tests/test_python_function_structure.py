b79ab1322c6ba61fe681142fd60010f49c6836ba
Louis Beal <louis.j.beal@gmail.com>
2025-09-18 10:40:16 +0200

    Add tests for valid structures

--- tests/test_python_function_structure.py
+++ tests/test_python_function_structure.py
@@ -11,6 +11,16 @@ from random import randint"""
 ]
 
 
+valid = [
+    """def generate_random_integers(n):
+    import random
+    return [random.randint(1, 100) for _ in range(n)]""",
+    """def generate_random_integers(n):
+    from random import randint
+    return [randint(1, 100) for _ in range(n)]"""
+]
+
+
 @pytest.mark.parametrize("case", invalid)
 def test_invalid_cases(case: str) -> None:
     print("Validating case:")
@@ -19,3 +29,13 @@ def test_invalid_cases(case: str) -> None:
 
     with pytest.raises(ValidationError):
         structure.model_validate({"name": "generate_random_integers", "source": case})
+
+
+@pytest.mark.parametrize("case", valid)
+def test_valid_cases(case: str) -> None:
+    print("Validating case:")
+    print(case)
+    structure = GeneratedFunction
+
+    validated = structure.model_validate({"name": "generate_random_integers", "source": case})
+    assert validated.source == case

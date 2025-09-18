4ad0cf52c2fadeaaec9f04e1ec5f8eb0c02d93f0
Louis Beal <louis.j.beal@gmail.com>
2025-09-18 13:35:38 +0200

    remove legacy testing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from openai import OpenAI
-from openai.types.chat.chat_completion import ChatCompletion
 
 from json_coerce.structures import GeneratedFunction
 from json_coerce.wrapper import StructuredWrapper
@@ -9,26 +8,6 @@ client = OpenAI(
     base_url="http://localhost:11434/v1",
     api_key="ollama",  # required, but unused
 )
-
-# "Mark is a 34 year old software engineer living in San Francisco."
-
-
-def generate_response(prompt: str, model: str, structure: str) -> ChatCompletion:
-    modified_prompt = f"""Extract details or perform tasks according to the following text:
-
-"{prompt}"
-
-Your response should contain ONLY a valid JSON object with the following fields.
-Do not respond with any other content, only the JSON object with the following fields:
-{structure}
-"""
-
-    completion = client.chat.completions.create(
-        model=model,
-        messages=[{"role": "user", "content": modified_prompt}],
-    )
-
-    return completion
 
 
 if __name__ == "__main__":

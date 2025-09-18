6a7538b63b27f43507fbca3b753a726544f82994
Louis Beal <louis.j.beal@gmail.com>
2025-09-18 16:08:04 +0200

    Wrapper class is recreated for each model

--- main.py
+++ main.py
@@ -34,8 +34,6 @@ if __name__ == "__main__":
         "mistral",
     ]
 
-    functionchat = StructuredWrapper(client, GeneratedFunction)
-
     prompt = "Write a Python function that returns n random characters from the hexadecimal set"
 
     # prompt = "Jason is a 30 year old software engineer living in San Francisco."
@@ -43,9 +41,12 @@ if __name__ == "__main__":
     print("### Testing generation with various models ###")
     print(f"models: {models}")
     print(f"User Prompt:\n{prompt}\n")
+
+    functionchat = StructuredWrapper(client, GeneratedFunction)
     print(f"Structure:\n{functionchat.structure}\n")
 
     for model in models:
+        functionchat = StructuredWrapper(client, GeneratedFunction)
         try:
             print(f"\n### Generating with model: {model}")
             result = functionchat.chat(prompt, model)
@@ -55,7 +56,10 @@ if __name__ == "__main__":
             # print(f"Name: {result.get('name')}")
             print(f"Source:\n{result.get('source')}")
         finally:
-            with open(f"chat_log_{model}.txt", "w") as f:
+            path = f"chat_log_{model}.txt"
+            print(f"\nSaving chat log to {path}")
+            print(f"Total messages: {len(functionchat.history)}")
+            with open(path, "w+") as f:
                 for i, message in enumerate(functionchat.history):
                     f.write(f"### Message {i + 1} ###\n")
                     f.write(f"{message['role'].upper()}:\n{message['content']}\n\n")

2bf966b1451b70c8951b58ed54c345aedc72e1f7
Louis Beal <louis.j.beal@gmail.com>
2025-09-18 17:31:28 +0200

    Add more tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -7,7 +7,13 @@ from json_coerce.json_parser import clean_output
 cases = [
     '{"name": "John", "age": 30}',  # Valid JSON,
     '{\n\t"name": "John",\n\t"age": 30\n}',  # JSON with newlines and tabs
+    '{\\n"name": "John",\\n"age": 30\\n}',  # JSON with newlines escaped
     "```json\n{\"name\": \"John\", \"age\": 30}\n```",  # JSON in markdown
+    """{
+  "name": 'John', "age": 30
+}
+
+The Python function"""  # '' quotes
 ]
 
 
